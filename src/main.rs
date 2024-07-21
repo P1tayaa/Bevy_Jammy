88bde7b80adf30496221455d3e1fffa84fad6013
omathot <116487276+omathot@users.noreply.github.com>
2024-07-21 18:13:02 +0200

    added player control hooks, physics engine, trunk in cargo.tom, attempting to push to main branch

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,51 @@
+use std::any::Any;
+
+use avian3d::prelude::*;
 use bevy::prelude::*;
+
+mod player;
+use player::{Player, PlayerPlugin};
+
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        // Enable physics
+        .add_plugins((DefaultPlugins, PlayerPlugin, PhysicsPlugins::default()))
         .add_systems(Startup, setup)
         .run();
 }
 
-/// set up a simple 3D scene
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // circular base
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Circle::new(4.0)),
-        material: materials.add(Color::WHITE),
-        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-        ..default()
-    });
-    // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        material: materials.add(Color::srgb_u8(124, 144, 255)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-    // light
+    // Static physics object with a collision shape
+    let player_id = commands.spawn((
+        RigidBody::Static,
+        Collider::cuboid(15., 0.1, 15.),
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(5., 0.1, 15.)),
+            material: materials.add(Color::WHITE),
+            ..default()
+        },
+    )).id();
+
+    // Dynamic physics object with a collision shape and initial angular velocity
+    commands.spawn((
+		Player,
+        RigidBody::Dynamic,
+        Collider::cuboid(1.0, 1.0, 1.0),
+        AngularVelocity(Vec3::new(2.5, 3.5, 1.5)),
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+            material: materials.add(Color::srgb_u8(124, 144, 255)),
+            transform: Transform::from_xyz(0.0, 4.0, 0.0),
+            ..default()
+        },
+    ));
+
+    // Light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
             shadows_enabled: true,
@@ -36,9 +54,10 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-    // camera
+
+    // Camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(0., 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
         ..default()
     });
 }
